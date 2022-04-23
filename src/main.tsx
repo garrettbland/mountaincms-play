d09e52716f544696cf3b7156ac47f579958b606d
Garrett Bland <gmorganbland@gmail.com>
2022-04-23 15:15:30 -0500

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Playground from './views/Playground'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Playground />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
---
+++ src/views/Playground.tsx
@@ -0,0 +1,10 @@
+export const Playground = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Mountain CMS</h1>
+      <p>DO stuff here</p>
+    </>
+  )
+}
+
+export default Playground
